ed0ff5967e62ca7e4ee690744c17dd25a51c1fd7
tanjil-islam <tanjil.islam040820@gmail.com>
2026-05-27 02:47:24 +0600

    Implement state and component lifecycle methods

---
+++ state_lifecycle/src/components/Clock.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+class Clock extends React.Component {
+  //   constructor(props) {
+  //     super(props)
+  //     this.state = {
+  //       date: new Date(),
+  //     }
+  //   }
+
+  state = {
+    date: new Date(),
+  }
+
+  componentDidMount() {
+    this.clockTimer = setInterval(() => this.tick(), 1000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.clockTimer)
+  }
+
+  tick() {
+    this.setState({
+      date: new Date(),
+    })
+  }
+
+  render() {
+    return (
+      <h1 className='heading'>
+        <span className='text'>
+          {this.state.date.toLocaleTimeString(this.props.locale)}
+        </span>
+      </h1>
+    )
+  }
+}
+
+export default Clock
