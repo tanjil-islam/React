f3dfac6b7c19e34a1a18df5b713762158b32ba2c
tanjil-islam <tanjil.islam040820@gmail.com>
2026-05-31 00:29:32 +0600

    Implement Higher-Order Components (HOC) logic reuse pattern

---
+++ higher_order_components/src/App.js
@@ -0,0 +1,13 @@
+import ClickCounter from './components/ClickCounter'
+import HoverCounter from './components/HoverCounter'
+
+function App() {
+  return (
+    <div className='App'>
+      <ClickCounter />
+      <HoverCounter />
+    </div>
+  )
+}
+
+export default App
