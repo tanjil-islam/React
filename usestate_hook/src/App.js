43a2d39a3d0ca8113970c40c15650709a07fe45d
tanjil-islam <tanjil.islam040820@gmail.com>
2026-06-10 00:54:19 +0600

    Implement state management using React useState hook

---
+++ usestate_hook/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Todo from './components/Todo'
+import Counter from './components/Counter'
+
+function App() {
+  return (
+    <div className='App'>
+      <Todo />
+      <Counter />
+    </div>
+  )
+}
+
+export default App
