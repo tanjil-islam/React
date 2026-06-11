b248d1ee24b904e207a84158ef28812c382d347d
tanjil-islam <tanjil.islam040820@gmail.com>
2026-06-11 18:45:47 +0600

    Implement CSS-in-JS architecture using styled-components

---
+++ css_in_js/src/App.js
@@ -0,0 +1,12 @@
+// import './assets/css/dark.css';
+import GlobalStyle from './components/styles/Global.styles'
+import Tutorial from './components/Tutorial'
+
+export default function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <Tutorial />
+    </>
+  )
+}
