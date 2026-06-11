581112898c5f3bdc4d17d20d5e10ad5ca0a486bc
tanjil-islam <tanjil.islam040820@gmail.com>
2026-06-11 14:14:36 +0600

    Implement styling via External CSS, Inline Styles, and CSS Modules

---
+++ stylesheets_inline_css/src/App.js
@@ -0,0 +1,13 @@
+import Logo from './components/Logo'
+import './assets/css/global.css'
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Welcome to Tanjil HUB</h1>
+      <Logo />
+    </div>
+  )
+}
+
+export default App
